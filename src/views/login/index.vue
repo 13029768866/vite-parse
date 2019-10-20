13c15e157ac140e0b1ad4785efc2feefeb3d364e
woodensRangerWZJ <13029768866@163.com>
2019-10-20 22:53:54 +0800

    登录页面骨架构建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,53 @@
 <template>
-	<div>登录</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-9 col-md-7 col-lg-5  m-auto pt-5">
+				<div class="card mt-5">
+					<div class="card-header bg-white">
+						<h3 class="text-center mb-0 text-info">vue-element-admin</h3>
+					</div>
+					<div class="card-body">
+						<el-form ref="form" :model="form">
+							<el-form-item>
+								<el-input 
+									v-model="form.username" 
+									size="medium"
+									placeholder="请输入用户名"
+								></el-input>								
+							</el-form-item>
+							<el-form-item>
+								<el-input 
+									v-model="form.username" 
+									size="medium"
+									type="password"
+									placeholder="请输入密码"
+								></el-input>								
+							</el-form-item>
+							<el-form-item>
+								<el-button
+									class="w-100"
+									type="primary" 
+									size="medium">立即登录</el-button>
+							</el-form-item>
+						</el-form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	export default {
+		data(){
+			return {
+				form:{
+					username:'',
+					password:''
+				}
+			}
+		}
+	}
 </script>
 
 <style>
