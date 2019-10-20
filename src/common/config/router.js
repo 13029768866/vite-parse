78e1a9672d95c34698a14cb1b6cc26f29e4df521
woodensRangerWZJ <13029768866@163.com>
2019-10-20 21:25:53 +0800

    根据component封装路由

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,3 +1,10 @@
+/* 
+	路由自动生成规则
+	* 一、例如：index/index，shop/index以index结尾的，path和name默认去除index
+	* 二、例如：shop/list，默认生成name为shop_list（如果结尾为index，例如shop/index则是shop）
+	* 三、填写后不会自动生成
+ */
+
 let routes = [
 	{
 		path: '/',
@@ -5,15 +12,15 @@ let routes = [
 		redirect:{name:'index'},
 		//component:()=>import('@/views/layout.vue'),
 		component: 'layout',
-		children: [{
-			path: '/index',
-			name: 'index',
+		children: [{			
 			component: 'index/index'
-		}]
+		},
+		{// 商品列表			
+			component:'shop/goods/list'
+		}
+		]
 	},
 	{//登录页
-		path: '/login',
-		name: 'login',
 		component: 'login/index'
 	},
 	{
@@ -33,6 +40,12 @@ function createRoute(arr){
 	for (let i = 0; i < arr.length; i++) {
 		// " * "重定向直接返回
 		if(!arr[i].component) return
+		// 去除index
+		let val = getPathVal(arr[i].component)
+		// 自动生成name
+		arr[i].name = arr[i].name || val.replace(/[/]/g,'_')
+		// 自动生成path
+		arr[i].path = arr[i].path || `/${val}`
 		// 自动生成component
 		let componentFun = import (`@/views/${arr[i].component}`)
 		arr[i].component = () => componentFun
@@ -41,6 +54,19 @@ function createRoute(arr){
 			createRoute(arr[i].children)
 		}
 	}
+}
+
+// 去除路由末尾index
+function getPathVal(str){
+	// 获取最后一个‘/’索引
+	let idx = str.lastIndexOf('/')
+	// 获取最后一个‘/’后面的参数
+	let val = str.substring(idx+1,str.length)
+	// 根据val进行相应处理
+	if( val === 'index'){
+		return str.substring(idx,-1)
+	}
+	return str
 }
 
 export default getRoutes()
