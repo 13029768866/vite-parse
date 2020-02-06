7fc14398799bcfb484004107fcdc89042d24aab9
woodensRangerWZJ <13029768866@163.com>
2020-02-06 14:07:44 +0800

    约定式路由封装

---
+++ src/pages/layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        主页面
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
