b4dd4e64d5e8cd7d32f16d5af73e3abbce67d05b
woodensRangerWZJ <13029768866@163.com>
2020-02-06 15:14:00 +0800

    登录页面构架

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -30,14 +30,14 @@ let manageRoutes = function () {
 
 /* 自动生成路由 */
 function autoCreateRouter(arr){
-    for ( let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++){
         if(!arr[i].component) return
-        /* 1、去除末尾index,生成name和path */
+        /*不设置自动生成name和path*/
         let val = manageIndex(arr[i].component)
-        arr[i].name = arr[i].name ||  val.replace(/\//g,'-')
+        arr[i].name = arr[i].name || val.replace(/\//g,'-')
         arr[i].path = arr[i].path || `/${val}`
 
-        /*2、生成component */
+        /* 自动生成component */
         let createComp = import(`../../pages/${arr[i].component}.vue`)
         arr[i].component = () => createComp
         if(arr[i].children && arr[i].children.length > 0){
@@ -49,11 +49,11 @@ function autoCreateRouter(arr){
 /* 处理index */
 function manageIndex(str) {
     let idx = str.lastIndexOf('/')
-    let val = str.substring(idx + 1)
+    let val = str.slice(idx + 1)
     if(val === 'index'){
-        return str.substring(idx, -1)
+        return str.substring(idx, -1 )
     }
-    return str
+    return  str
 }
 
 export default manageRoutes()
