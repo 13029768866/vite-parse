30dea53b1f8599d7d1d00581b9c307dde3edaada
woodensRangerWZJ <13029768866@163.com>
2021-06-18 14:37:43 +0800

    [refactor]: refactor init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import viteESLint from '@ehutch79/vite-eslint';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteESLint(
+      { 'include': ['src/**/*.vue', 'src/**/*.js','src/**/*.ts'] }
+    )
+  ],
+});
