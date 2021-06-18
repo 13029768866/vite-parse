00baa2849487f3916fd686073701eff44237d96d
woodensRangerWZJ <13029768866@163.com>
2021-06-18 17:45:43 +0800

    [feat]: lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import vue from '@vitejs/plugin-vue';
 import viteESLint from '@ehutch79/vite-eslint';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteESLint(
-      { 'include': ['src/**/*.vue', 'src/**/*.js','src/**/*.ts'] }
-    )
-  ],
+  plugins: [vue(), viteESLint({ include: ['src/**/*.vue', 'src/**/*.js', 'src/**/*.ts'] })],
 });
